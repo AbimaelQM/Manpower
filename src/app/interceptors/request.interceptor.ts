309c5897f9d202cbab2769c4e1ce91ada2884e09
AbimaelQM <60951913+AbimaelQM@users.noreply.github.com>
2022-11-06 11:49:35 -0500

    Deploy heroku request for back

--- src/app/interceptors/request.interceptor.ts
+++ src/app/interceptors/request.interceptor.ts
@@ -22,19 +22,19 @@ export class RequestInterceptor implements HttpInterceptor {
 
   ) { }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
 
     const cloneRequest = request.clone({
 
-      withCredentials: true,
-      setHeaders: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '/*',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-        'Access-Control-Allow-Headers':
-          'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
-      }
+      withCredentials: true
+      // setHeaders: {
+      //   'Content-Type': 'application/json',
+      //   'Access-Control-Allow-Origin': '/*',
+      //   'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+      //   'Access-Control-Allow-Headers':
+      //   'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+      // }
 
     });
 
